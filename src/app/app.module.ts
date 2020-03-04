2364b8423477d659535b02d4284e0b3dd59f669a
0016118 <0016118@insags.com>
2020-03-04 10:35:04 +0100

    First tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StrengthPipe } from './pipes/strength.pipe';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StrengthPipe,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
